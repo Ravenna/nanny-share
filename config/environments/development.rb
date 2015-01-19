60162e02a3fa7fe0330fa6bc4053698f34dc8234
ravenna <tj@ravennainteractive.com>
2015-01-19 14:39:48 -0800

    update action mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,5 +30,16 @@ NannyShare::Application.configure do
   
   # ACTION MAILER
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+
+    config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :domain               => 'seattlenanny.com',
+     :user_name            => 'info@seattlenanny.com',
+     :password             => 'info0704',
+     :authentication       => :plain,
+     :enable_starttls_auto => true  
+  }
   
 end
