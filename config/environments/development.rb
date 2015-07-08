1cec595d9d0247375827feb08f2fb4c4355bb616
ravenna <tj@ravennainteractive.com>
2015-07-08 11:55:15 -0700

    update email password for all env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ NannyShare::Application.configure do
      :port                 => '587',
      :domain               => 'seattlenanny.com',
      :user_name            => 'admin@seattlenanny.com',
-     :password             => 'Admi2590',
+     :password             => 'Rapunzel.216!',
      :authentication       => :login, 
      :enable_starttls_auto => true
      
